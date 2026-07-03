db5ec75f5b5a48e4e4552a23725d43b4e8779af2
Venkata Saran Somisetty <saranlastname@gmail.com>
2026-07-04 00:07:49 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <vector>
 
+// ---------------------------------------------------------
+// CORE UTILITIES
+// ---------------------------------------------------------
 uint64_t read_var(std::ifstream &database_file) {
   uint64_t readval = 0;
   uint8_t ip;
@@ -43,8 +46,8 @@ uint32_t get_table_root_page(std::ifstream &db,
     uint16_t celloff = read_integer(db, 2);
 
     db.seekg(celloff);
-    read_var(db);
-    read_var(db);
+    read_var(db); // Payload Size
+    read_var(db); // RowID
 
     uint64_t header_start = db.tellg();
     uint64_t size_of_header = read_var(db);
@@ -56,8 +59,6 @@ uint32_t get_table_root_page(std::ifstream &db,
 
     uint64_t type_size = (serial_type - 13) / 2;
     uint64_t name_size = (serial_name - 13) / 2;
-
-    // type Type 1 = 1 byte (int8), Type 2 = 2 bytes (int16), Type 3 = 3 bytes
     uint64_t rootpage_size =
         (serial_rootpage >= 1 && serial_rootpage <= 4) ? serial_rootpage : 0;
 
@@ -75,7 +76,8 @@ uint32_t get_table_root_page(std::ifstream &db,
       uint64_t tbl_name_size = (serial_tbl_name - 13) / 2;
       db.seekg(data_start + type_size + name_size + tbl_name_size);
 
-      return read_integer(db, rootpage_size);
+      uint32_t root_page = read_integer(db, rootpage_size);
+      return root_page;
     }
   }
   return 0;
@@ -92,14 +94,16 @@ uint64_t count_table_rows(std::ifstream &db, uint32_t page_num,
   db.seekg(offset + 3);
   uint16_t num_cells = read_integer(db, 2);
 
+  // Base Case: Leaf Page (0x0D)
   if (page_type == 0x0D) {
     return num_cells;
   }
 
+  // Recursive Case: Interior Page (0x05)
   uint64_t total_rows = 0;
   if (page_type == 0x05) {
     for (int i = 0; i < num_cells; i++) {
-      db.seekg(offset + 12 + (i * 2)); // Interior headers are 12 bytes long
+      db.seekg(offset + 12 + (i * 2));
       uint16_t cell_ptr = read_integer(db, 2);
 
       db.seekg(offset + cell_ptr);
@@ -108,7 +112,6 @@ uint64_t count_table_rows(std::ifstream &db, uint32_t page_num,
       total_rows += count_table_rows(db, left_child_page, page_size);
     }
 
-    // Interior pages have "Right-most pointer" at offset 8
     db.seekg(offset + 8);
     uint32_t rightmost_page = read_integer(db, 4);
     total_rows += count_table_rows(db, rightmost_page, page_size);
@@ -118,6 +121,7 @@ uint64_t count_table_rows(std::ifstream &db, uint32_t page_num,
 }
 
 int main(int argc, char *argv[]) {
+  // Required by CodeCrafters to ensure output isn't buffered
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -133,7 +137,47 @@ int main(int argc, char *argv[]) {
   database_file.seekg(16);
   uint16_t page_size = read_integer(database_file, 2);
 
-  if (command.find("SELECT COUNT(*)") != std::string::npos) {
+  if (command == ".dbinfo") {
+    std::cout << "database page size: " << page_size << std::endl;
+    database_file.seekg(103);
+    uint16_t num_tables = read_integer(database_file, 2);
+    std::cout << "number of tables: " << num_tables << std::endl;
+  }
+
+  else if (command == ".tables") {
+    database_file.seekg(103);
+    uint16_t num_cells = read_integer(database_file, 2);
+
+    for (unsigned int pt = 0; pt < num_cells; pt++) {
+      database_file.seekg(108 + pt * 2);
+      uint16_t celloff = read_integer(database_file, 2);
+
+      database_file.seekg(celloff);
+      read_var(database_file);
+      read_var(database_file);
+
+      uint64_t header_start = database_file.tellg();
+      uint64_t size_of_header = read_var(database_file);
+      uint64_t serial_type = read_var(database_file);
+      uint64_t serial_name = read_var(database_file);
+
+      uint64_t type_size = (serial_type - 13) / 2;
+      uint64_t name_size = (serial_name - 13) / 2;
+
+      database_file.seekg(header_start + size_of_header);
+      std::string type_string(type_size, '\0');
+      database_file.read(&type_string[0], type_size);
+
+      database_file.seekg(header_start + size_of_header + type_size);
+      std::string table_name(name_size, '\0');
+      database_file.read(&table_name[0], name_size);
+
+      if (type_string == "table" && table_name != "sqlite_sequence") {
+        std::cout << table_name << " ";
+      }
+    }
+    std::cout << std::endl;
+  } else if (command.find("SELECT COUNT(*)") != std::string::npos) {
     std::string target_table = command.substr(command.find_last_of(' ') + 1);
 
     uint32_t root_page = get_table_root_page(database_file, target_table);
@@ -143,7 +187,7 @@ int main(int argc, char *argv[]) {
           count_table_rows(database_file, root_page, page_size);
       std::cout << total_rows << std::endl;
     } else {
-      std::cerr << "Table not found!" << std::endl;
+      std::cerr << "[ERROR] get_table_root_page returned 0!" << std::endl;
     }
   }
 
